c08bd9881bdabfbe7771c8afd04d7a0af4b09e1f
Avdhesh Yadav <avdhesh7394vn@gmail.com>
2019-01-31 13:12:25 +0530

    Comments

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const token = 'NTM5Mzc2NDAxMzg0NjAzNjQ4.DzBc_A.AjOVEyXD7IDB-7vsTQJjuu8_oE0';
 
 lifeBot.registry.registerGroup('lifegoals', 'Life');
 lifeBot.registry.registerDefaults();
-lifeBot.registry.registerCommandsIn(__dirname + '/commands');
+lifeBot.registry.registerCommandsIn(__dirname + '/commands');//
 
 lifeBot.login(token);
 
